b04941b3afb432d4dfc53e5dd7c7d65c9e0b68ba
Alexis Lara <alexislaracamacho@my.whccd.edu>
2022-11-02 20:17:03 -0700

    stuff

---
+++ script2.js
@@ -0,0 +1,8 @@
+let aVariable1 = "Hello";
+//console.log(aVariable1);
+aVariable1 = "World!";
+//console.log(aVariable1)
+
+const thisIsANewVariable = Math.max(5, 10) * 10 - 10;
+Console.log(Math.max(5, 10));
+
---
+++ script3.js
@@ -0,0 +1,11 @@
+let myGreeting = "Hello World!";
+
+console.log(myGreeting);
+
+const pi = 3.14;
+
+//pi = 2;
+
+let myNumber = 222;
+
+console.log(Math.min(myNumber, pi));
